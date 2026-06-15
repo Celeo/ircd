663462486988f9acbb095cb584d8867c34aba67a
Celeo <mattboulanger@fastmail.com>
2026-06-14 20:40:00 -0700

    reinit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#![deny(clippy::all)]
+#![deny(clippy::pedantic)]
+#![deny(unsafe_code)]
+
 use clap::Parser;
-use log::{debug, info};
 use std::env;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
@@ -10,7 +13,7 @@ use tokio::{
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Enable debug logging to a 'vatsim_online.log' file
+    /// Enable debug logging
     #[clap(short, long)]
     debug: bool,
 
@@ -24,37 +27,14 @@ struct Cli {
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
-    if cli.debug {
-        env::set_var("RUST_LOG", "debug");
-    } else if env::var("RUST_LOG").is_err() {
-        env::set_var("RUST_LOG", "info");
-    }
-    pretty_env_logger::init();
 
-    debug!("Starting");
+    // ...
 
     let address = format!("{}:{}", cli.host, cli.port);
     let listener = TcpListener::bind(&address).await.unwrap();
-    info!("Listening on {address}");
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process_socket(socket).await;
+        // ...
     }
 }
 
-async fn process_socket(mut socket: TcpStream) {
-    let mut buf = vec![0; 1024];
-    loop {
-        let n = socket.read(&mut buf).await.unwrap();
-        if n == 0 {
-            info!("Read 0 bytes; disconnecting client");
-            break;
-        }
-        let read = &buf[0..n];
-        let read = std::str::from_utf8(read).unwrap();
-        if read.len() > 2 {
-            info!(": {}", read.trim());
-            _ = socket.write(b"I hear you\n").await.unwrap();
-        }
-    }
-}
