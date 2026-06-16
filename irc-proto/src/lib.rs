e1a8192c2ea5f5382bf084f0f52849889d5f2415
Celeo <mattboulanger@fastmail.com>
2026-06-15 21:58:37 -0700

    Start

--- irc-proto/src/lib.rs
+++ irc-proto/src/lib.rs
@@ -1,1 +1,31 @@
+//! IRC protocol message parsing and serialization.
 
+#![deny(clippy::all)]
+#![deny(clippy::pedantic)]
+#![deny(unsafe_code)]
+#![deny(missing_docs)]
+
+use std::collections::HashMap;
+
+/// IRC message.
+pub struct Message {
+    /// Message metadata.
+    pub tags: HashMap<String, Option<String>>,
+    /// Message origin.
+    pub source: Option<String>,
+    /// IRC command or numeric.
+    pub command: String,
+    /// Command arguments.
+    pub parameters: Vec<String>,
+}
+
+/// Protocol errors.
+#[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
+pub enum ProtoError {
+    //
+}
+
+///
+pub fn parse_message(message: &str) -> Result<Message, ProtoError> {
+    todo!()
+}
